5a70c1115d4c602bbda3daef8ab2b187db866eb0
xiaoxiao529111 <xil084@ucsd.edu>
2021-10-08 13:08:11 -0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { Button, Col, Layout, message, Row } from 'antd';
+import { Button, Col, Layout, Menu, message, Row } from 'antd';
 import Login from './components/Login';
 import Register from './components/Register';
-import { logout } from './utils';
+import { getTopGames, logout } from './utils';
 import Favorites from './components/Favorites';
+import { LikeOutlined, FireOutlined } from '@ant-design/icons';
+import CustomSearch from './components/CustomSearch';
+import SubMenu from 'antd/lib/menu/SubMenu';
  
 const { Header, Content, Sider } = Layout;
  
 class App extends React.Component {
   state = {
-    loggedIn: false
+    loggedIn: false,
+    topGames: []
   }
  
   signinOnSuccess = () => {
@@ -26,6 +30,18 @@ class App extends React.Component {
         })
         message.success(`Successfull signed out`);
       }).catch((err) => {
+        message.error(err.message);
+      })
+  }
+ 
+  componentDidMount = () => {
+    getTopGames()
+      .then((data) => {
+        this.setState({
+          topGames: data
+        })
+      })
+      .catch((err) => {
         message.error(err.message);
       })
   }
@@ -57,7 +73,33 @@ class App extends React.Component {
       </Header>
       <Layout>
         <Sider width={300} className="site-layout-background">
-          {'Sider'}
+          <CustomSearch />
+          <Menu
+            mode="inline"
+            onSelect={() => {}}
+  		 style={{ marginTop: '10px' }}
+          >
+            <Menu.Item icon={<LikeOutlined />} key="Recommendation">
+              Recommend for you!</Menu.Item>
+            <SubMenu icon={<FireOutlined />} key="Popular Games" title="Popular Games" className="site-top-game-list" >
+              {
+                this.state.topGames.map((game) => {
+                  return (
+                    <Menu.Item key={game.id} style={{ height: '50px' }}>
+                      <img 
+                        alt="Placeholder"
+                        src={game.box_art_url.replace('{height}', '40').replace('{width}', '40')}
+                        style={{ borderRadius: '50%', marginRight: '20px' }}
+                      />
+                      <span>
+                        {game.name}
+                      </span>
+                    </Menu.Item>
+                  )
+                })
+              }
+            </SubMenu>
+          </Menu>
         </Sider>
         <Layout style={{ padding: '24px' }}>
           <Content
@@ -69,7 +111,7 @@ class App extends React.Component {
               overflow: 'auto'
             }}
           >
-            {'Home'} 
+            {'Home'}
           </Content>
         </Layout>
       </Layout>
@@ -78,5 +120,3 @@ class App extends React.Component {
 }
  
 export default App;
-
-
