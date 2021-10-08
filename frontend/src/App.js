86065c3f9cb59f2897cbb9c42ecfbeee3d3e8fd1
xiaoxiao529111 <xil084@ucsd.edu>
2021-10-07 22:10:27 -0700

    update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,48 @@
 import React from 'react';
-import { Layout } from 'antd';
+import { Button, Layout, message } from 'antd';
+import Login from './components/Login';
+import Register from './components/Register';
+import { logout } from './utils';
  
 const { Header, Content, Sider } = Layout;
  
 class App extends React.Component {
+  state = {
+    loggedIn: false
+  }
+ 
+  signinOnSuccess = () => {
+    this.setState({
+      loggedIn: true
+    })
+  }
+ 
+  signoutOnClick = () => {
+    logout()
+      .then(() => {
+        this.setState({
+          loggedIn: false
+        })
+        message.success(`Successfull signed out`);
+      }).catch((err) => {
+        message.error(err.message);
+      })
+  }
+ 
   render = () => (
     <Layout>
       <Header>
-        {'Header'}
+        {
+          this.state.loggedIn ? 
+          <Button shape="round" onClick={this.signoutOnClick}>
+            Logout</Button> :
+          (
+            <>
+              <Login onSuccess={this.signinOnSuccess} />
+              <Register />
+            </>
+          )
+        }
       </Header>
       <Layout>
         <Sider width={300} className="site-layout-background">
@@ -32,4 +67,3 @@ class App extends React.Component {
 }
  
 export default App;
-
