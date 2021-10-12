ae2e40208734999ab987c94bac5250cf4fd3ce34
xiaoxiao529111 <xil084@ucsd.edu>
2021-10-12 13:40:31 -0700

    Game.java Created

--- backend/src/main/java/com/laioffer/jupiter/GameServlet.java
+++ backend/src/main/java/com/laioffer/jupiter/GameServlet.java
@@ -1,19 +1,50 @@
 package com.laioffer.jupiter;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import org.apache.commons.io.IOUtils;
+import org.json.JSONObject;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet(name = "GameServlet", urlPatterns = {"/game"})
 public class GameServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().print("Hello World");
+        response.setContentType("application/json");
+        JSONObject game = new JSONObject();
+        game.put("name", "World of Warcraft");
+        game.put("developer", "Blizzard Entertainment");
+        game.put("release_time", "Feb 11, 2005");
+        game.put("website", "https://www.worldofwarcraft.com");
+        game.put("price", 49.99);
+
+        // Write game information to response body
+        response.getWriter().print(game);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        JSONObject jsonRequest = new JSONObject(IOUtils.toString(request.getReader()));
+        String name = jsonRequest.getString("name");
+        String developer = jsonRequest.getString("developer");
+        String releaseTime = jsonRequest.getString("release_time");
+        String website = jsonRequest.getString("website");
+        float price = jsonRequest.getFloat("price");
+        // Print game information to IDE console
+        System.out.println("Name is: " + name);
+        System.out.println("Developer is: " + developer);
+        System.out.println("Release time is: " + releaseTime);
+        System.out.println("Website is: " + website);
+        System.out.println("Price is: " + price);
+        // Return status = ok as response body to the client
+        response.setContentType("application/json");
+        JSONObject jsonResponse = new JSONObject();
+        jsonResponse.put("status", "ok");
+        response.getWriter().print(jsonResponse);
 
     }
 }
---
+++ backend/src/main/java/com/laioffer/jupiter/entity/Game.java
@@ -0,0 +1,5 @@
+package com.laioffer.jupiter.entity;
+
+public class Game {
+
+}
