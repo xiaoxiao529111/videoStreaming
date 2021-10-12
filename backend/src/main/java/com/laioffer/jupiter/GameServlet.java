d209332b579f5ef2806179eda8a93269356eda75
xiaoxiao529111 <xil084@ucsd.edu>
2021-10-12 14:07:39 -0700

    Update Game.java

--- backend/src/main/java/com/laioffer/jupiter/GameServlet.java
+++ backend/src/main/java/com/laioffer/jupiter/GameServlet.java
@@ -1,5 +1,7 @@
 package com.laioffer.jupiter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.laioffer.jupiter.entity.Game;
 import org.apache.commons.io.IOUtils;
 import org.json.JSONObject;
 
@@ -14,16 +16,21 @@ import java.io.IOException;
 public class GameServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("application/json");
-        JSONObject game = new JSONObject();
-        game.put("name", "World of Warcraft");
-        game.put("developer", "Blizzard Entertainment");
-        game.put("release_time", "Feb 11, 2005");
-        game.put("website", "https://www.worldofwarcraft.com");
-        game.put("price", 49.99);
 
-        // Write game information to response body
-        response.getWriter().print(game);
+
+        response.setContentType("application/json");
+        ObjectMapper mapper = new ObjectMapper();
+        Game.Builder builder = new Game.Builder();
+        builder.setName("World of Warcraft");
+        builder.setDeveloper("Blizzard Entertainment");
+        builder.setReleaseTime("Feb 11, 2005");
+        builder.setWebsite("https://www.worldofwarcraft.com");
+        builder.setPrice(49.99);
+
+
+        Game game = builder.build();
+        response.getWriter().print(mapper.writeValueAsString(game));
+
     }
 
     @Override
