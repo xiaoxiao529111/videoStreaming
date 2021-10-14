684c878a5ba03780b861e191467d58f4dce9ae5f
xiaoxiao529111 <xil084@ucsd.edu>
2021-10-13 22:13:06 -0700

    Updates

---
+++ backend/src/main/java/com/laioffer/jupiter/servlet/GameServlet.java
@@ -0,0 +1,24 @@
+package com.laioffer.jupiter.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "GameServlet", urlPatterns = {"/game"})
+public class GameServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+    }
+}
