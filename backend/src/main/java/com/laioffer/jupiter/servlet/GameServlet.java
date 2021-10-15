a07ebe39b1d474f25eafd74728abb682f21d2059
xiaoxiao529111 <xil084@ucsd.edu>
2021-10-15 10:22:06 -0700

    Update GameServlet.java

--- backend/src/main/java/com/laioffer/jupiter/servlet/GameServlet.java
+++ backend/src/main/java/com/laioffer/jupiter/servlet/GameServlet.java
@@ -1,5 +1,9 @@
 package com.laioffer.jupiter.servlet;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.laioffer.jupiter.external.TwitchClient;
+import com.laioffer.jupiter.external.TwitchException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,9 +15,23 @@ import java.io.IOException;
 public class GameServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Get gameName from request URL.
+        String gameName = request.getParameter("game_name");
+        TwitchClient client = new TwitchClient();
 
-
-
+        // Let the client know the returned data is in JSON format.
+        response.setContentType("application/json;charset=UTF-8");
+        try {
+            // Return the dedicated game information if gameName is provided in the request URL, otherwise return the top x games.
+            if (gameName != null) {
+                response.getWriter().print(new ObjectMapper().writeValueAsString(client.searchGame(gameName)));
+            } else {
+                response.getWriter().print(new ObjectMapper().writeValueAsString(client.topGames(0)));
+            }
+        } catch (TwitchException e) {
+            throw new ServletException(e);
+        }
+        
     }
 
     @Override
