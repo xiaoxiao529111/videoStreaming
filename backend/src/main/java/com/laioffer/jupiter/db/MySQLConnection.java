d094f335abc24126da04c1eb93572ac320391c0e
xiaoxiao529111 <xil084@ucsd.edu>
2021-10-16 19:37:18 -0700

    update db

---
+++ backend/src/main/java/com/laioffer/jupiter/db/MySQLConnection.java
@@ -0,0 +1,96 @@
+package com.laioffer.jupiter.db;
+
+import com.laioffer.jupiter.entity.Item;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class MySQLConnection {
+    private final Connection conn;
+
+    // Create a connection to the MySQL database.
+    public MySQLConnection() throws MySQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+            conn = DriverManager.getConnection(MySQLDBUtil.getMySQLAddress());
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new MySQLException("Failed to connect to Database");
+        }
+    }
+
+    public void close() {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // Insert a favorite record to the database
+    public void setFavoriteItem(String userId, Item item) throws MySQLException {
+        if (conn == null) {
+            System.err.println("DB connection failed");
+            throw new MySQLException("Failed to connect to Database");
+        }
+        // Need to make sure item is added to the database first because the foreign key restriction on item_id(favorite_records) -> id(items)　
+        saveItem(item);
+        // Using ? and preparedStatement to prevent SQL injection
+        String sql = "INSERT IGNORE INTO favorite_records (user_id, item_id) VALUES (?, ?)";
+        try {
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setString(1, userId);
+            statement.setString(2, item.getId());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new MySQLException("Failed to save favorite item to Database");
+        }
+    }
+
+    // Remove a favorite record from the database
+    public void unsetFavoriteItem(String userId, String itemId) throws MySQLException {
+        if (conn == null) {
+            System.err.println("DB connection failed");
+            throw new MySQLException("Failed to connect to Database");
+        }
+        String sql = "DELETE FROM favorite_records WHERE user_id = ? AND item_id = ?";
+        try {
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setString(1, userId);
+            statement.setString(2, itemId);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new MySQLException("Failed to delete favorite item to Database");
+        }
+    }
+
+    // Insert an item to the database.
+    public void saveItem(Item item) throws MySQLException {
+        if (conn == null) {
+            System.err.println("DB connection failed");
+            throw new MySQLException("Failed to connect to Database");
+        }
+        String sql = "INSERT IGNORE INTO items VALUES (?, ?, ?, ?, ?, ?, ?)";
+        try {
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setString(1, item.getId());
+            statement.setString(2, item.getTitle());
+            statement.setString(3, item.getUrl());
+            statement.setString(4, item.getThumbnailUrl());
+            statement.setString(5, item.getBroadcasterName());
+            statement.setString(6, item.getGameId());
+            statement.setString(7, item.getType().toString());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new MySQLException("Failed to add item to Database");
+        }
+    }
+
+}
--- backend/src/main/java/com/laioffer/jupiter/db/MySQLException.java
+++ backend/src/main/java/com/laioffer/jupiter/db/MySQLException.java
@@ -1,4 +1,8 @@
 package com.laioffer.jupiter.db;
 
-public class MySQLException {
+public class MySQLException extends RuntimeException {
+    public MySQLException(String errorMessage) {
+        super(errorMessage);
+    }
 }
+
