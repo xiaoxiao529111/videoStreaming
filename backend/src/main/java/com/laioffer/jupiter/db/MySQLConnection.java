15651e723083da18a31fd4546ac851444732e64a
xiaoxiao529111 <xil084@ucsd.edu>
2021-10-18 12:08:33 -0700

    Create favorite servlet

--- backend/src/main/java/com/laioffer/jupiter/db/MySQLConnection.java
+++ backend/src/main/java/com/laioffer/jupiter/db/MySQLConnection.java
@@ -1,11 +1,10 @@
 package com.laioffer.jupiter.db;
 
 import com.laioffer.jupiter.entity.Item;
+import com.laioffer.jupiter.entity.ItemType;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.*;
 
 public class MySQLConnection {
     private final Connection conn;
@@ -91,6 +90,89 @@ public class MySQLConnection {
             e.printStackTrace();
             throw new MySQLException("Failed to add item to Database");
         }
+    }
+
+    // Get favorite item ids for the given user
+    public Set<String> getFavoriteItemIds(String userId) throws MySQLException {
+        if (conn == null) {
+            System.err.println("DB connection failed");
+            throw new MySQLException("Failed to connect to Database");
+        }
+
+        Set<String> favoriteItems = new HashSet<>();
+        String sql = "SELECT item_id FROM favorite_records WHERE user_id = ?";
+        try {
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setString(1, userId);
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                String itemId = rs.getString("item_id");
+                favoriteItems.add(itemId);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new MySQLException("Failed to get favorite item ids from Database");
+        }
+
+        return favoriteItems;
+    }
+
+    // Get favorite items for the given user. The returned map includes three entries like {"Video": [item1, item2, item3], "Stream": [item4, item5, item6], "Clip": [item7, item8, ...]}
+    public Map<String, List<Item>> getFavoriteItems(String userId) throws MySQLException {
+        if (conn == null) {
+            System.err.println("DB connection failed");
+            throw new MySQLException("Failed to connect to Database");
+        }
+        Map<String, List<Item>> itemMap = new HashMap<>();
+        for (ItemType type : ItemType.values()) {
+            itemMap.put(type.toString(), new ArrayList<>());
+        }
+        Set<String> favoriteItemIds = getFavoriteItemIds(userId);
+        String sql = "SELECT * FROM items WHERE id = ?";
+        try {
+            PreparedStatement statement = conn.prepareStatement(sql);
+            for (String itemId : favoriteItemIds) {
+                statement.setString(1, itemId);
+                ResultSet rs = statement.executeQuery();
+                if (rs.next()) {
+                    ItemType itemType = ItemType.valueOf(rs.getString("type"));
+                    Item item = new Item.Builder().id(rs.getString("id")).title(rs.getString("title"))
+                            .url(rs.getString("url")).thumbnailUrl(rs.getString("thumbnail_url"))
+                            .broadcasterName(rs.getString("broadcaster_name")).gameId(rs.getString("game_id")).type(itemType).build();
+                    itemMap.get(rs.getString("type")).add(item);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new MySQLException("Failed to get favorite items from Database");
+        }
+        return itemMap;
+    }
+    // Get favorite game ids for the given user. The returned map includes three entries like {"Video": ["1234", "5678", ...], "Stream": ["abcd", "efgh", ...], "Clip": ["4321", "5678", ...]}
+    public Map<String, List<String>> getFavoriteGameIds(Set<String> favoriteItemIds) throws MySQLException {
+        if (conn == null) {
+            System.err.println("DB connection failed");
+            throw new MySQLException("Failed to connect to Database");
+        }
+        Map<String, List<String>> itemMap = new HashMap<>();
+        for (ItemType type : ItemType.values()) {
+            itemMap.put(type.toString(), new ArrayList<>());
+        }
+        String sql = "SELECT game_id, type FROM items WHERE id = ?";
+        try {
+            PreparedStatement statement = conn.prepareStatement(sql);
+            for (String itemId : favoriteItemIds) {
+                statement.setString(1, itemId);
+                ResultSet rs = statement.executeQuery();
+                if (rs.next()) {
+                    itemMap.get(rs.getString("type")).add(rs.getString("game_id"));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new MySQLException("Failed to get favorite game ids from Database");
+        }
+        return itemMap;
     }
 
 }
---
+++ backend/src/main/java/com/laioffer/jupiter/servlet/FavoriteServlet.java
@@ -0,0 +1,48 @@
+package com.laioffer.jupiter.servlet;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.laioffer.jupiter.db.MySQLConnection;
+import com.laioffer.jupiter.db.MySQLException;
+import com.laioffer.jupiter.entity.FavoriteRequestBody;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "FavoriteServlet", value = "/FavoriteServlet")
+public class FavoriteServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Get user ID from request URL, this is a temporary solution since we don’t support session now
+        String userId = request.getParameter("user_id");
+        // Get favorite item information from request body
+        ObjectMapper mapper = new ObjectMapper();
+        FavoriteRequestBody body = mapper.readValue(request.getReader(), FavoriteRequestBody.class);
+        if (body == null) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        MySQLConnection connection = null;
+        try {
+            // Save the favorite item to the database
+            connection = new MySQLConnection();
+            connection.setFavoriteItem(userId, body.getFavoriteItem());
+        } catch (MySQLException e) {
+            throw new ServletException(e);
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+        }
+
+    }
+}
