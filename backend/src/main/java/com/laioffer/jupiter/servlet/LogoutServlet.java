cf76b7ce7331bdde504a630ed35823971d160eaf
xiaoxiao529111 <xil084@ucsd.edu>
2021-10-18 17:39:59 -0700

    Login, logout

---
+++ backend/src/main/java/com/laioffer/jupiter/servlet/LogoutServlet.java
@@ -0,0 +1,20 @@
+package com.laioffer.jupiter.servlet;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "LogoutServlet", urlPatterns = {"/logout"})
+public class LogoutServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Destroy the session since the user is logged out.
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+    }
+
+}
